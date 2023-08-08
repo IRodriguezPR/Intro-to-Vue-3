c1f2f159e6ea6c9cfba1f87a492a2144bbfc9a07
IRodriguezPR <israelpr@gmail.com>
2023-08-08 15:59:12 -0400

    Done

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks',
-            image: './assets/images/socks_green.jpg',
-            // solution
-            url: 'https://www.vuemastery.com/'
-            // solution
+            product: 'Socks'
         }
     }
 })
